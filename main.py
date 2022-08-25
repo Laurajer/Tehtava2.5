f57c3770322998cfb5ad6b7bf47cd1fe69cf3bd8
Omistaja <laura.erelaakso@metropolia.fi>
2022-08-25 19:30:46 +0300

    test

--- main.py
+++ main.py
@@ -3,16 +3,18 @@
 # 1 naula on 32 luotia
 # 1 luoti on 13,3g
 
-leiviska_string = input("Anna leiviskät:")
-naula_string = input("Anna naulat:")
-luoti_string = input("Anna luodit:")
-leiviska = float(leiviska_string)
-naula = float(naula_string)
-luoti = float(luoti_string)
+leiviska_string = input("Anna leiviskät: ")
+naula_string = input("Anna naulat: ")
+luoti_string = input("Anna luodit: ")
+leiviska = float(leiviska_string) * 8512
+naula = float(naula_string) * 425.6
+luoti = float(luoti_string) * 13.3
 # Luoti = 13,3g
 # Naula = 13,3g * 32 = 425,6g
 # Leiviskä = 425,6g * 20 = 8512g
-area = luoti * 13.3
-area1 =
+massa = luoti + naula + leiviska
+yhteispaino = massa / 1000
+yhteispaino_int = int(yhteispaino)
+ylijaama = int((yhteispaino - int(yhteispaino)) * 1000)
 
-print(f"Massa nykymittojen mukaan on {area: .3f} ")
+print("Massa nykymittojen mukaan on " + str(yhteispaino_int) + " kilogrammaa ja " + str(ylijaama) + " grammaa.")
